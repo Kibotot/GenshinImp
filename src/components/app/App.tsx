e471b1549b94eb60f88893c4d19ae7fdd5ce8cbe
jurica.hrg <jurica.hrg@brainit.agency>
2021-01-23 10:50:31 +0100

    Inital changes, init project files

---
+++ src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+};
---
+++ src/components/character/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export const Character = () => {
+  return (
+    <div className={styles.root}>
+      <span>char</span>
+    </div>
+  );
+};
